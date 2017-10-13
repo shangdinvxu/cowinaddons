ef3e7ac5881330c0ea24fe6a5c9ec5ce1696a81a
Abner <2906631584@qq.com>
2017-10-13 11:51:32 +0800

    添加员工序列所需要的配置信息  修改菜单折叠的信息

--- cowin_hr/__manifest__.py
+++ cowin_hr/__manifest__.py
@@ -29,6 +29,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/sequences.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
