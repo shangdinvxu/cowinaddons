3dd284103fce745315b4ff914bb6b54fe89ef686
Abner <2906631584@qq.com>
2017-10-17 16:37:02 +0800

    测试对用户登录的设置

--- cowin_hr/models/models.py
+++ cowin_hr/models/models.py
@@ -11,7 +11,13 @@ class Cowin_hr(models.Model):
     identification_id_contract_person = fields.Char(string=u'身份证号')
     mobile_phone_backup_contract_person = fields.Char(string=u'联系电话')
 
-    barcode = fields.Char()
+    barcode = fields.Char(string=u'员工编码')
+
+    # is_add_user=fields.Boolean()
+    # login_name=fields.Char()
+    login = fields.Char(related='user_id.login')
+
+
 
 
     @api.model
@@ -19,7 +25,23 @@ class Cowin_hr(models.Model):
         if not vals.get('barcode'):
             vals['barcode'] = self.env['ir.sequence'].next_by_code('cowin_hr.order')
 
-        return super(Cowin_hr, self).create(vals)
+
+        # 创建当前的hr.employee实例
+        res_hr=super(Cowin_hr, self).create(vals)
+
+
+        # # 开始对外键进行设置
+        # if vals.get('login_name'):
+        #     # 这样的命名的规则是从字段的命名规则来考虑
+        #     user_id=self.env['res.users'].create({
+        #         'login': 'nam',
+        #         'pass':'123455'
+        #
+        #     })
+        #
+        #     res_hr.user_id=user_id.id
+        #     res_hr.address_home_id=user_id.partner_id.id
+        return res
 
 
 class IrMenuExtend(models.Model):
