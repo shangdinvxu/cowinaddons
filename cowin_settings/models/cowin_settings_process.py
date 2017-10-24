1e50fcf88240bcdd8b0168e1b2c21c9e71c161db
Abner <2906631584@qq.com>
2017-10-24 17:44:54 +0800

    update

--- cowin_settings/models/cowin_settings_process.py
+++ cowin_settings/models/cowin_settings_process.py
@@ -82,7 +82,7 @@ class Cowin_settings_process(models.Model):
 
     # 使用rpc方法来对删除分组所对应的实例(记录)
     def rpc_delete_group(self, **kwargs):
-        id = kwargs.get('stage_id')
+        id = int(kwargs.get('stage_id'))
         self.env['cowin_settings.process_stage'].browse(id).unlink()
 
         return self.get_info()
@@ -105,7 +105,7 @@ class Cowin_settings_process(models.Model):
 
     # 使用rpc来删除环节记录(实体)
     def rpc_delete_tache(self, **kwargs):
-        id = kwargs.get('tache_id')
+        id = int(kwargs.get('tache_id'))
         self.env['cowin_settings.process_tache'].browse(id).unlink()
 
         return self.get_info()
