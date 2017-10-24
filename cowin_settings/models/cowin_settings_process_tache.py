d17494647e1a388bc7e479e885e6d1b503de336d
jason.lu@linkloving.com <Nt5211314!@#>
2017-10-24 12:16:08 +0800

    'project'

--- cowin_settings/models/cowin_settings_process_tache.py
+++ cowin_settings/models/cowin_settings_process_tache.py
@@ -15,3 +15,23 @@ class Cowin_settings_process_tache(models.Model):
     state = fields.Boolean(string=u'启用状态', default=True)
 
     stage_id = fields.Many2one('cowin_settings.process_stage', ondelete="cascade")
+
+    def _check_parent_id(self, parent_id, ids):
+        parent = self.env['cowin_settings.process_tache'].browse(parent_id)
+        if parent:
+            if ids.contains(parent):
+                return False
+            if self.id != parent.id:
+                if parent.parent_id:
+                    ids.append(self.id)
+                    ids.append(parent.id)
+                    parent._check_parent_id(ids)
+                else:
+                    return True
+            else:
+                return False
+
+    def on_set_parent_id(self, parent_id):
+        ids = []
+        if self._check_parent_id(self, parent_id, ids):
+            self.parent_id = parent_id
