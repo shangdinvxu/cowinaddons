4a244fc329088d5d18a94d2017483ffcd7edf9ea
wangkefu <745376631@qq.com>
2017-10-24 11:05:18 +0800

    update

--- cowin_settings/static/src/js/process_conf_detail.js
+++ cowin_settings/static/src/js/process_conf_detail.js
@@ -18,7 +18,32 @@ odoo.define('cowin_settings.process_conf_detail', function (require) {
 
     var ProcessConfDetail = Widget.extend({
         events:{
-
+            'click .new_group_button':'create_new_group_func',
+            'click .alia_cancel':'close_create_wrap',
+            'click .alia_confirm':'confirm_create_group'
+        },
+        //新建分组
+        confirm_create_group:function (e) {
+            var e = e || window.event;
+            var target = e.target || e.srcElement;
+            var self = this;
+            if($('.create_group_input').val().length==0){
+                Dialog.alert(target, "分组名不能为空！");
+                return;
+            }
+            new Model("cowin_settings.process_stage")
+                    .call("rpc_create_group", [[]], {name:$('.create_group_input').val(),process_id:self.id})
+                    .then(function (result) {
+                        console.log(result);
+                        $('.create_new_group').hide();
+                    })
+        },
+        close_create_wrap:function () {
+            $(".close_this_container").hide()
+        },
+        create_new_group_func:function () {
+            console.log('ssssssss');
+            $(".create_new_group").show();
         },
         init: function (parent, action) {
             this._super.apply(this, arguments);
