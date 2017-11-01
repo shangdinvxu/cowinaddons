2920974d49c1ab70c7839b377cee166bc04c5adc
wangkefu <745376631@qq.com>
2017-11-01 08:54:35 +0800

    update

--- cowin_project/models/__init__.py
+++ cowin_project/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
+from . import cowin_foundation
 from . import cowin_project
 from . import cowin_project_task
 from . import cowin_common
 from . import cowin_project_opinion_book
-from . import cowin_foundation
