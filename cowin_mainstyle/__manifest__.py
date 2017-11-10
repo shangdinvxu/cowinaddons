1e1ba7ec55843693981a0b674f2468bfc77ce3cd
wangkefu <745376631@qq.com>
2017-11-10 11:02:48 +0800

    update

--- cowin_mainstyle/__manifest__.py
+++ cowin_mainstyle/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/data.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/webclient_templates.xml',
