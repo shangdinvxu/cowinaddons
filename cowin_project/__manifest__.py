84af077ff4a54346bb45eff943b768da89cb492b
Abner <2906631584@qq.com>
2017-11-13 11:41:59 +0800

    添加子工程form2

--- cowin_project/__manifest__.py
+++ cowin_project/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/sub_project/sub_project_establishment.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
