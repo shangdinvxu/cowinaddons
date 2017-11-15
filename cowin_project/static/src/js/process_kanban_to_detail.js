cee07a2d866de816d0c2fd9d2d5570d42b072ba9
wangkefu <745376631@qq.com>
2017-11-15 08:42:28 +0800

    update

--- cowin_project/static/src/js/process_kanban_to_detail.js
+++ cowin_project/static/src/js/process_kanban_to_detail.js
@@ -26,6 +26,7 @@ odoo.define('cowin_project.process_kanban_to_detail', function (require) {
         initiate_func:function (e) {
             var e = e || window.event;
             var target = e.target || e.srcElement;
+            var tache_index = $(target).parents('.process_data_item_line').attr('tache-index');
             var tache_id = parseInt($(target).parents('.process_data_item_line').attr('data-tache-id'));
 
             var self = this;
@@ -33,7 +34,7 @@ odoo.define('cowin_project.process_kanban_to_detail', function (require) {
                 view_type: 'form',
                 view_mode: 'form',
                 views: [[false, 'form']],
-                res_model: 'cowin_project.cowin_subproject',
+                res_model: self.model_arr[parseInt(tache_index)],
                 context: {'project_id': self.id,"tache_id":tache_id},
                 type: 'ir.actions.act_window',
                 target:'new'
@@ -44,13 +45,21 @@ odoo.define('cowin_project.process_kanban_to_detail', function (require) {
             this._super.apply(this, arguments);
             this.id = action.id;
             var self = this;
+            //存储每个环节的model_name
+            self.model_arr = [];
         },
         start: function () {
             var self = this;
             return new Model("cowin_project.cowin_project")
                     .call("rpc_get_info", [self.id])
                     .then(function (result) {
-                        console.log(result);
+                        //获取每个环节的model_name存入数组
+                        result.process.forEach(function (value) {
+                            value.tache_ids.forEach(function (model) {
+                               self.model_arr.push(model.model_name)
+                            });
+                        });
+
                         self.id = parseInt(result.id);
                         self.$el.append(QWeb.render('project_process_detail_tmp', {result: result}))
                     })
