927ceb0e23318294095e514eeeaa46788bce3464
Abner <2906631584@qq.com>
2017-12-01 15:21:28 +0800

    update3

--- cowin_project/models/settings/approval_settings/cowin_sub_project_approval_flow_settings_records.py
+++ cowin_project/models/settings/approval_settings/cowin_sub_project_approval_flow_settings_records.py
@@ -14,7 +14,7 @@ class Cowin_sub_project_approval_flow_settings_records(models.Model):
         审批记录!!!
     '''
 
-
+    sub_approval_flow_id = fields.Many2one('', string=u'子审批实体', ondelete="cascade")
     user_id = fields.Many2one('hr.employee', string=u'审批人')
     approval_role = fields.Many2one('cowin_common.approval_role', string=u'审批角色')
 
