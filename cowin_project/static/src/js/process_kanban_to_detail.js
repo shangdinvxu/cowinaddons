2f5c0df11c2bae8514b1c2a0b1c924e38ec99603
wangkefu <745376631@qq.com>
2018-01-08 09:14:19 +0800

    update

--- cowin_project/static/src/js/process_kanban_to_detail.js
+++ cowin_project/static/src/js/process_kanban_to_detail.js
@@ -365,7 +365,7 @@ odoo.define('cowin_project.process_kanban_to_detail', function (require) {
 
 
             //测试
-            return new Model("cowin_project.cowin_project")
+            new Model("cowin_project.cowin_project")
                     .call("rpc_load_and_return_action", [parseInt(self.id)],{'tache_info':self.tache_arr[tache_index]})
                     .then(function (result) {
                         result.context['tache'] = self.tache_arr[tache_index];
@@ -418,41 +418,41 @@ odoo.define('cowin_project.process_kanban_to_detail', function (require) {
             // };
             // self.do_action(action);
             //
-            // // ajax监听事件 用以刷新页面
-            // var sub_id = $('.active_fund').attr('data-sub-id');
-            // var refresh_page = null;
-            // refresh_page = function (self,model) {
-            //     $(document).ajaxComplete(function (event, xhr, settings) {
-            //         if (settings.data){
-            //             var data = JSON.parse(settings.data);
-            //             if (data.params.model && data.params.model == model) {
-            //                 if (data.params.method == 'create' || data.params.method == 'write'){
-            //                     $('.close').click(function () {
-            //                         return new Model("cowin_project.cowin_project")
-            //                             .call("rpc_get_info", [parseInt(self.id)],{meta_project_id:parseInt(sub_id)})
-            //                             .then(function (result) {
-            //                                 result.no_initate = self.no_initate
-            //                                 console.log(result);
-            //                                 $('.process_data_main_wrap').html('');
-            //                                 // 获取每个环节的model_name存入数组
-            //                                 self.tache_arr = [];
-            //                                 result.process.forEach(function (value) {
-            //                                     value.tache_ids.forEach(function (model) {
-            //                                         self.tache_arr.push((model))
-            //                                     });
-            //                                 });
-            //                                 self.id = parseInt(result.id);
-            //                                 $('.process_data_main_wrap').append(QWeb.render('process_info_right_tmpl', {result: result}));
-            //                                 $('.process_funds_rounds').html('');
-            //                                 $('.process_funds_rounds').append(QWeb.render('process_info_left_tmpl', {result: result,active_flag:sub_id}));
-            //                             })
-            //                     })
-            //                 }
-            //             }
-            //         }
-            //     })
-            // }
-            // refresh_page(self,self.tache_arr[tache_index].model_name);
+            // ajax监听事件 用以刷新页面
+            var sub_id = $('.active_fund').attr('data-sub-id');
+            var refresh_page = null;
+            refresh_page = function (self,model) {
+                $(document).ajaxComplete(function (event, xhr, settings) {
+                    if (settings.data){
+                        var data = JSON.parse(settings.data);
+                        if (data.params.model && data.params.model == model) {
+                            if (data.params.method == 'create' || data.params.method == 'write'){
+                                $('.close').click(function () {
+                                    return new Model("cowin_project.cowin_project")
+                                        .call("rpc_get_info", [parseInt(self.id)],{meta_project_id:parseInt(sub_id)})
+                                        .then(function (result) {
+                                            result.no_initate = self.no_initate
+                                            console.log(result);
+                                            $('.process_data_main_wrap').html('');
+                                            // 获取每个环节的model_name存入数组
+                                            self.tache_arr = [];
+                                            result.process.forEach(function (value) {
+                                                value.tache_ids.forEach(function (model) {
+                                                    self.tache_arr.push((model))
+                                                });
+                                            });
+                                            self.id = parseInt(result.id);
+                                            $('.process_data_main_wrap').append(QWeb.render('process_info_right_tmpl', {result: result}));
+                                            $('.process_funds_rounds').html('');
+                                            $('.process_funds_rounds').append(QWeb.render('process_info_left_tmpl', {result: result,active_flag:sub_id}));
+                                        })
+                                })
+                            }
+                        }
+                    }
+                })
+            }
+            refresh_page(self,self.tache_arr[tache_index].model_name);
         },
 
         init: function (parent, action, options) {
