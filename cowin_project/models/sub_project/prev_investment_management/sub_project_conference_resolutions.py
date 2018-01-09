e9342682d3984941efaa2c037b18c70095beab9d
Abner <2906631584@qq.com>
2018-01-09 14:51:14 +0800

    未做投票结果  将要切换新的分支

--- cowin_project/models/sub_project/prev_investment_management/sub_project_conference_resolutions.py
+++ cowin_project/models/sub_project/prev_investment_management/sub_project_conference_resolutions.py
@@ -19,6 +19,17 @@ class Cowin_project_subproject_conference_resolutions(models.Model):
     # invest_manager_id = fields.Many2one('hr.employee', string=u'投资经理')
     invest_manager_ids = fields.Many2many('hr.employee', string=u'投资经理')
 
+    # ----------  投资基金
+    round_financing_and_foundation_id = fields.Many2one('cowin_project.round_financing_and_foundation',
+                                                        related='subproject_id.round_financing_and_foundation_id',
+                                                        string=u'基金轮次实体')
+    round_financing_id = fields.Many2one('cowin_common.round_financing', string=u'融资轮次')
+    foundation_id = fields.Many2one('cowin_foundation.cowin_foudation', string=u'基金名称')
+    the_amount_of_financing = fields.Float(string=u'本次融资金额')
+    the_amount_of_investment = fields.Float(string=u'本次投资金额')
+    ownership_interest = fields.Integer(string=u'股份比例')
+    # ----------
+
     voting_committee = fields.Date(string=u'投决会日期')
 
     members_of_voting_committee_ids = fields.Many2many('hr.employee', string=u'投决会委员')
@@ -100,6 +111,11 @@ class Cowin_project_subproject_conference_resolutions(models.Model):
         # tem = meta_sub_project_entity.project_id.copy_data()[0]
 
         common_fileds = [
+            'round_financing_id',
+            'foundation_id',
+            'the_amount_of_financing',
+            'the_amount_of_investment',
+            'ownership_interest',
         ]
 
         common_fileds.extend(['name', 'project_number', 'invest_manager_id'])
