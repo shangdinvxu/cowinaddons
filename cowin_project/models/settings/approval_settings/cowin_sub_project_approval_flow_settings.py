047242978a713c3a40f4ed5a6b569559902a5ed3
Abner <2906631584@qq.com>
2018-02-07 15:02:52 +0800

    添加用户操作名

--- cowin_project/models/settings/approval_settings/cowin_sub_project_approval_flow_settings.py
+++ cowin_project/models/settings/approval_settings/cowin_sub_project_approval_flow_settings.py
@@ -99,6 +99,7 @@ class Cowin_sub_project_approval_flow_settings(models.Model):
         tmp2[u'round_financing_name'] = round_financing_name
         tmp2[u'foundation_name'] = foundation_name
         tmp2[u'approval_role_name'] = approval_role_name
+        tmp2[u'approval_role_person'] = self.env.user.employee_ids[0].name_related
         # tmp2[u'approval_roel_person'] = approval_roel_person
         tmp2[u'operation_time'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + 8 * 3600))
         tmp2[u'operation'] = u''
