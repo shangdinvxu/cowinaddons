8732e5f20e3652ecef8843f3525ad8ebb5644975
Abner <2906631584@qq.com>
2018-03-26 17:18:02 +0800

    自定义footer, 修改方法调用链

--- cowin_foundation/models/cowin_foundation_GP.py
+++ cowin_foundation/models/cowin_foundation_GP.py
@@ -67,3 +67,13 @@ class Cowin_foundation_GP(models.Model):
             res.append(tmp)
 
         return res
+
+    @api.multi
+    def button_save(self):
+        '''
+            这个方法不要调用,原理在于每次button调用之前都会提前走write方法!!!
+        :return:
+        '''
+        self.ensure_one()
+
+        pass
